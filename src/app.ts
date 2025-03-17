cf6823e2052c3bfa3c27aa3b888cd7ed93dce64e
Marion Queen Ramos <mramos3@academic.rrc.ca>
2025-03-17 09:40:29 -0500

    chore: updated app.ts to include env configuration.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import moderationRoutes from "./api/v1/routes/moderationRoutes";
+import config from './config';
 
 const app: Express = express();
 app.use(express.json());
 
 /**
- * Mount moderation routes on /api/v1/moderation
+ * Mount moderation routes with versioned path from config
  */
-app.use("/api/v1/moderation", moderationRoutes);
+app.use(`/api/${config.API.VERSION}/moderation`, moderationRoutes);
 
 /**
  * Default error handler for unmatched routes
  */
 app.use((req: Request, res: Response): void => {
-	res.status(404).json({ message: "Endpoint not found" });
+  res.status(404).json({ message: "Endpoint not found" });
 });
 
 export default app;
